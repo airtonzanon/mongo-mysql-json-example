b5735585f922a22ea19fe76a6c750ef2570ad151
Ivan Rosolen <ivan.rosolen@arizona.com.br>
2017-09-27 17:50:58 -0300

    mongo and mysql

--- conn.php
+++ conn.php
@@ -1,9 +1,16 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+// mysql
 try {
     $dbh = new PDO('mysql:host=mysql;dbname=remembender', 'remembender', 'remembender');
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
-} 
+}
+
+// mongo
+$db = new \MongoDB\Client('mongodb://mongodb:27017');
+$collection = $db->selectDatabase('ivan');
